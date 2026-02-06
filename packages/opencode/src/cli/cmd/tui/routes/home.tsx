e90314dd903825ddc9c004927e3e267e10348171
Trevor <trevorr2615169@gmail.com>
2026-02-06 13:05:28 -0500

    style: center ulmcode banner with prompt container alignment

--- packages/opencode/src/cli/cmd/tui/routes/home.tsx
+++ packages/opencode/src/cli/cmd/tui/routes/home.tsx
@@ -95,7 +95,9 @@ export function Home() {
     <>
       <box flexGrow={1} justifyContent="center" alignItems="center" paddingLeft={2} paddingRight={2} gap={1}>
         <box height={3} />
-        <Logo />
+        <box width="100%" maxWidth={75} alignItems="center">
+          <Logo />
+        </box>
         <box width="100%" maxWidth={75} zIndex={1000} paddingTop={1}>
           <Prompt
             ref={(r) => {
